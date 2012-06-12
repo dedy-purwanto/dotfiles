0fd748f64be4c64174887887bf67eaf752c28d3e
Dedi Purwanto <kecebongsoft@gmail.com>
2012-06-12 06:47:11 +0000

    rearange link script executions

--- link.sh
+++ link.sh
@@ -7,6 +7,6 @@ ln -f .hgrc ~/
 ln -f .Xmodmap ~/
 ln -f .Xresources ~/
 
-sh gnome-terminal-colors-solarized/set_dark.sh
 ln -f ls-colors-solarized/dircolors ~/.dircolors
 ln -f urxvt-tabbedex/tabbedex ~/.urxvt
+sh gnome-terminal-colors-solarized/set_dark.sh
