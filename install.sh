066b3fc357c907eb802100fa7f5fdb8058a8dd82
Dedi Purwanto <kecebongsoft@gmail.com>
2014-09-07 20:43:52 +0800

    Use softlink with absolute path for installation

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 while true; do
-    read -p "Do you wish to install this program? (y/n) : " yn
+    read -p "Do you wish to link dotfiles?. WARNING: It will override your existing dotfiles, please check openup this script to see the full list of overrides. (y/n) : " yn
     case $yn in
         [Yy]* ) 
-            cp zshrc ~/.zshrc;
-            cp vimrc ~/.vimrc;
-            cp gitconfig ~/.gitconfig;
-            cp hgrc ~/.hgrc;
-            cp minimal.zsh-theme ~/.oh-my-zsh/themes/;
+            ln -s -f `pwd`/zshrc ~/.zshrc;
+            ln -s -f `pwd`/vimrc ~/.vimrc;
+            ln -s -f `pwd`/gitconfig ~/.gitconfig;
+            ln -s -f `pwd`/hgrc ~/.hgrc;
+            ln -s -f `pwd`/minimal.zsh-theme ~/.oh-my-zsh/themes/;
             break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
