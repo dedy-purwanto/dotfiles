f9a78ab8f04c5deb47e02b0e176bfef8295b05d2
Dedi Purwanto <kecebongsoft@gmail.com>
2014-10-29 12:53:56 +0800

    Removed wrong grammar

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    read -p "Do you wish to link dotfiles?. WARNING: It will override your existing dotfiles, please check openup this script to see the full list of overrides. (y/n) : " yn
+    read -p "Do you wish to link dotfiles? WARNING: It will override your existing dotfiles, please check openup this script to see the full list of overrides. (y/n) : " yn
     case $yn in
         [Yy]* ) 
             ln -s -f `pwd`/zshrc ~/.zshrc;
