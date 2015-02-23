cb2a2f2a1f3a210209c9a9be05ca673e9d182e7d
Dedi Purwanto <kecebongsoft@gmail.com>
2015-02-23 17:37:37 +0800

    Install with no confirmation

--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-while true; do
-    read -p "Do you wish to link dotfiles? WARNING: It will override your existing dotfiles, please check openup this script to see the full list of overrides. (y/n) : " yn
-    case $yn in
-        [Yy]* ) 
-            ln -s -f `pwd`/zshrc ~/.zshrc;
-            ln -s -f `pwd`/vimrc ~/.vimrc;
-            ln -s -f `pwd`/gitconfig ~/.gitconfig;
-            ln -s -f `pwd`/hgrc ~/.hgrc;
-            ln -s -f `pwd`/minimal.zsh-theme ~/.oh-my-zsh/themes/;
-            ln -s -f `pwd`/tmux.conf ~/.tmux.conf;
-            ln -s -f `pwd`/tmuxinator/* ~/.tmuxinator/;
-            ln -s -f `pwd`/dircolors ~/.dircolors;
-            break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+ln -s -f `pwd`/zshrc ~/.zshrc;
+ln -s -f `pwd`/vimrc ~/.vimrc;
+ln -s -f `pwd`/gitconfig ~/.gitconfig;
+ln -s -f `pwd`/hgrc ~/.hgrc;
+ln -s -f `pwd`/minimal.zsh-theme ~/.oh-my-zsh/themes/;
+ln -s -f `pwd`/tmux.conf ~/.tmux.conf;
+ln -s -f `pwd`/tmuxinator/* ~/.tmuxinator/;
+ln -s -f `pwd`/dircolors ~/.dircolors;
